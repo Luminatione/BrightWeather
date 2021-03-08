e40b7194eb1d4bf111c17d33464a086af74035eb
Luminatione <patryklazowski@hotmail.com>
2021-03-08 22:54:46 +0100

    Updated unit tests

--- BrightWeatherUnitTests/BrightWeatherUnitTests.cpp
+++ BrightWeatherUnitTests/BrightWeatherUnitTests.cpp
@@ -23,28 +23,26 @@ namespace BrightWeatherUnitTests
 		TEST_METHOD(TestWeekDays)
 		{
 			//some simple tests
-			Assert::IsTrue(areEqual({ "Monday", "Tuesday", "Wednesday", "Thursday" }, WeekDaysNames::GetDaysFromTo(1, 4)));
-			Assert::IsTrue(areEqual({ "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }, WeekDaysNames::GetDaysFromTo(2, 7)));
-			Assert::IsTrue(areEqual({ "Friday", "Saturday", "Sunday" }, WeekDaysNames::GetDaysFromTo(5, 7)));
-			Assert::IsTrue(areEqual({ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }, WeekDaysNames::GetDaysFromTo(1, 7)));
+			Assert::IsTrue(areEqual({ "Monday", "Tuesday", "Wednesday", "Thursday" }, WeekDaysNames::GetDaysFromTo(0, 3)));
+			Assert::IsTrue(areEqual({ "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }, WeekDaysNames::GetDaysFromTo(1, 6)));
+			Assert::IsTrue(areEqual({ "Friday", "Saturday", "Sunday" }, WeekDaysNames::GetDaysFromTo(4, 6)));
+			Assert::IsTrue(areEqual({ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }, WeekDaysNames::GetDaysFromTo(0, 6)));
 			//harder ones
-			Assert::IsTrue(areEqual({ "Monday", "Tuesday", "Wednesday" }, WeekDaysNames::GetDaysFromTo(8, 10)));
-			Assert::IsTrue(areEqual({ "Friday", "Saturday", "Sunday", "Monday", "Tuesday" }, WeekDaysNames::GetDaysFromTo(12, 16)));
+			Assert::IsTrue(areEqual({ "Monday", "Tuesday", "Wednesday" }, WeekDaysNames::GetDaysFromTo(7, 9)));
+			Assert::IsTrue(areEqual({ "Friday", "Saturday", "Sunday", "Monday", "Tuesday" }, WeekDaysNames::GetDaysFromTo(11, 15)));
 			Assert::IsTrue(areEqual({ "Saturday", "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
-			"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday","Monday", "Tuesday", "Wednesday", "Thursday" }, WeekDaysNames::GetDaysFromTo(20, 40)));
+			"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday","Monday", "Tuesday", "Wednesday", "Thursday" }, WeekDaysNames::GetDaysFromTo(19, 39)));
 			//limit tests
-			Assert::IsTrue(areEqual({ "Monday" }, WeekDaysNames::GetDaysFromTo(1, 1)));
-			Assert::IsTrue(areEqual({ "Sunday" }, WeekDaysNames::GetDaysFromTo(7, 7)));
-			Assert::IsTrue(areEqual({ "Monday" }, WeekDaysNames::GetDaysFromTo(50, 50)));
+			Assert::IsTrue(areEqual({ "Monday" }, WeekDaysNames::GetDaysFromTo(0, 0)));
+			Assert::IsTrue(areEqual({ "Sunday" }, WeekDaysNames::GetDaysFromTo(6, 6)));
+			Assert::IsTrue(areEqual({ "Monday" }, WeekDaysNames::GetDaysFromTo(49, 49)));
 			//exceptions tests
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, -1, 12);
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, 4, -16);
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, -2, -5);
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, 5, 4);
-			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, 0, 0);
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, -4, 0);
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, 6, 0);
-			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, 0, 5);
 			EXCEPTION_TEST(WeekDaysNames::GetDaysFromTo, 0, -7);			
 		}
 		TEST_METHOD(WeekDayUtility)//function is hard to test due to its dependency on current date, useful test would have to have it's own tests, the fastest way to check it is to debug test manually
