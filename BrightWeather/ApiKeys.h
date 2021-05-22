3eca60a6197d7ddcb2d69c36dc6751aac39de063
Luminatione <41484837+Luminatione@users.noreply.github.com>
2021-05-22 19:33:08 +0200

    Create ApiKeys.h

---
+++ BrightWeather/ApiKeys.h
@@ -0,0 +1,2 @@
+static const std::string apiKeyOpenWeatherMap = place your https:\/\/openweathermap.org/ api key here;
+static const std::string apiKeyIPInfo = place your https:\/\/ipinfo.io/ api key here;
